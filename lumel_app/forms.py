e8c302d98ee2444eacadec7ba345a0fad5445374
malaury lemaitre salmon <malaury.lemaitresalmon@sciencespo.fr>
2024-10-30 10:44:11 +0100

    update seo

--- lumel_app/forms.py
+++ lumel_app/forms.py
@@ -11,15 +11,6 @@ class CustomTimeInput(forms.TimeInput):
 
 
 class ContactForm(forms.Form):
-    EXPERTISES = [
-        ('', 'Expertises'),
-        ('Droit Immobilier', 'Droit Immobilier'),
-        ('Droit Social', 'Droit Social'),
-        ('Réparation du dommage corporel', 'Réparation du dommage corporel'),
-        ('Droit Patrimonial', 'Droit Patrimonial'),
-        ('Droit Commercial', 'Droit Commercial'),
-        ('Autre', 'Autre'),
-    ]
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
@@ -29,9 +20,6 @@ class ContactForm(forms.Form):
     nom = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Nom'}))
     telephone = forms.CharField(required=False, max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Téléphone'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail'}))
-    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    heure = forms.TimeField(label='Heure', widget=CustomTimeInput(), initial=time(10, 30))
-    expertise = forms.ChoiceField(choices=EXPERTISES, required=True, widget=forms.Select(attrs={'class': 'form-control'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'rows': 4, 'cols': 40}))
 
 
