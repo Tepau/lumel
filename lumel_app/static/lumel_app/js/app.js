2328429a45698559d61be8a6484b835173cd546f
CCC <axel.durand3@gmail.com>
2024-12-02 15:57:35 +0100

    Content change + fixs + optimisations

--- lumel_app/static/lumel_app/js/app.js
+++ lumel_app/static/lumel_app/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     addLozad();
     AOS.init();
+    addVHandVWValues();
 });
 
 // LAZY LOAD
@@ -8,4 +9,21 @@ const addLozad = () => {
     const observer = lozad(); 
     observer.observe();
 }  
+
+const addVHandVWValues = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    const vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
+
+
+    window.addEventListener("resize", () => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+        const vw = window.innerWidth * 0.01;
+        document.documentElement.style.setProperty("--vw", `${vw}px`);
+    });
+};
 
